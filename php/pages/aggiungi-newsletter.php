1001b8c2e3ea5e319faa6c135f1391f0911a97e3
Samuele Gardin <43345662+Smule97@users.noreply.github.com>
2018-12-14 17:44:46 +0100

    Update aggiungi-newsletter.php

--- php/pages/aggiungi-newsletter.php
+++ php/pages/aggiungi-newsletter.php
@@ -1,20 +1,30 @@
 <?php
 	include("../connection.php");
-	$email = $_POST['email'];
-	$stmt = $conn->prepare"SELECT COUNT(*) AS nRighe FROM utente WHERE Email = '".$email."'";
-	$stmt->execute();
-	$result = $stmt->fetch(PDO::FETCH_ASSOC);
-	$nRighe=$result['nRighe'];
-	if($nRighe==0){
-		$stmt = $conn->prepare"INSERT INTO utente (Email, NewsLetter) VALUES ('".$email."', 'true')";
-		$stmt->execute();
-	}
-	else{
-		$stmt = $conn->prepare"SELECT ID FROM utente WHERE Email = '".$email."'";
+	
+	if(isset($_POST['email'])){
+		$email = $_POST['email'];
+		$stmt = $conn->prepare"SELECT COUNT(*) AS nRighe FROM Utente WHERE Email = ".$email.";";
 		$stmt->execute();
 		$result = $stmt->fetch(PDO::FETCH_ASSOC);
-		$id=$result['ID'];
-		$stmt = $conn->prepare"UPDATE SET NewsLetter=true WHERE ID = '".$id."'";
-		$stmt->execute();
+		//print_r($result);
+		$nRighe = $result['nRighe'];
+		if($nRighe == 0){
+			$stmt = $conn->prepare"INSERT INTO Utente (Email, NewsLetter) VALUES (".$email.", true);";
+			$stmt->execute();
+		}
+		else{
+			$stmt = $conn->prepare"SELECT ID FROM Utente WHERE Email = ".$email.";";
+			$stmt->execute();
+			$result = $stmt->fetch(PDO::FETCH_ASSOC);
+			$id = $result['ID'];
+			$stmt = $conn->prepare"UPDATE Utente SET NewsLetter=true WHERE ID = ".$id.";";
+			$stmt->execute();
+		}
+		header('http://localhost/info-e-contatti');
+	}
+	else{
+		print_r("value è (non settato) ");
+		print_r($value);
+		header('http://localhost/info-e-contatti');
 	}
 ?>
