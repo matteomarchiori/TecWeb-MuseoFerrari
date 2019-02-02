ef91159142b2745e74defc65ef9b423fe3a3d8c7
Smule97 <samuelegardin1997@gmail.com>
2019-02-02 16:09:41 +0100

    Aggiunto js a info-e-contatti

---
+++ mamarchi/js/info-e-contatti.js
@@ -0,0 +1,41 @@
+function checkCampo(input){
+	if(input[1].test(input[0].value)){
+		togliErrore(input[0]);
+	}
+	else{
+		mostraErrore(input[0], input[2]);
+	}
+}
+
+function mostraErrore(input, testo){
+    togliErrore(input);
+	var p = input.parentNode;
+	var paragraph = document.createElement("p");
+	paragraph.className = "error";
+    paragraph.setAttribute("id","javascript");
+	paragraph.appendChild(document.createTextNode(testo));
+	p.appendChild(paragraph);
+}
+
+function togliErrore(input){
+	var p = input.parentNode;
+	//alert(p.children.length);
+	if(p.children.length > 2){
+		p.removeChild(p.children[2]);
+	}
+}
+
+function validazione(input){
+	switch (input.id) {
+		case 'nome':
+			var patt = new RegExp('^[a-zA-Z]{1,15}$');
+			var nome = [input, patt, "Il campo Nome inserito non è corretto. Rispettare il formato indicato."];
+			checkCampo(nome);
+			break;
+		case 'email':
+			var patt = new RegExp('^[a-zA-Z0-9.:_-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,4}$');
+			var email = [input, patt, "Il campo Email inserito non è corretto. Rispettare il formato indicato."];
+			checkCampo(email);
+			break;
+	}
+}
