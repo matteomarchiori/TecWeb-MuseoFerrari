8e4279675a2133f855319a2c8e79562b65001ffa
Perry15 <peron.giovanni@gmail.com>
2019-02-09 12:20:20 +0100

    aggiunta icona, sistemato bottone torna su e link nei pulsanti vari

--- mamarchi/php/database/database.php
+++ mamarchi/php/database/database.php
@@ -5,8 +5,8 @@ namespace Database;
 class Database {
 
     const HOST_DB = "localhost";
-    const USERNAME = "matteo";
-    const PASSWORD = "password1324354";
+    const USERNAME = "root";
+    const PASSWORD = "root";
     const DB_NAME = "museo-ferrari";
 
     private static $connection;
