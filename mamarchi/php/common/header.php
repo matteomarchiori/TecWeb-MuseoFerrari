645b290f4a7243c0852b907831c6d9b566d35d42
Matteo <matteo.marchiori97@gmail.com>
2019-02-13 11:27:22 +0100

    aggiunti meta tags

--- mamarchi/php/common/header.php
+++ mamarchi/php/common/header.php
@@ -17,32 +17,32 @@ switch ($last_uri_parts[0]){
         break;
     case "mostre":
         $header = str_replace("*title*","Mostre | Museo Ferrari",$header);
-        $header = str_replace("*description*","",$header);
-        $header = str_replace("*keywords*","",$header);
+        $header = str_replace("*description*","Nella pagina delle mostre è presente una descrizione della mostra Il cavallino degli anni '50 e l'elenco delle prossime mostre che si terranno al museo",$header);
+        $header = str_replace("*keywords*","cavallino,mostre,museo",$header);
         $header = str_replace("*linkmostre*","<li id='currentLink'>Mostre</li>",$header);
         $header = str_replace("*breadcrumbs*","Mostre",$header);
         $page = dirname(__DIR__).DIRECTORY_SEPARATOR."pages".DIRECTORY_SEPARATOR."mostre.php";
         break;
     case "modelli-esposti":
         $header = str_replace("*title*","Modelli Esposti | Museo Ferrari",$header);
-        $header = str_replace("*description*","",$header);
-        $header = str_replace("*keywords*","",$header);
+        $header = str_replace("*description*","Tutti i modelli Ferrari esposti della collezione del museo, con i dettagli relativi a motore, cilindrata, potenza ed esposizione nella mostra in corso.",$header);
+        $header = str_replace("*keywords*","Ferrari,motore,cilindrata",$header);
         $header = str_replace("*linkmodelli*","<li id='currentLink'>Modelli esposti</li>",$header);
         $header = str_replace("*breadcrumbs*","Modelli Esposti",$header);
         $page = dirname(__DIR__).DIRECTORY_SEPARATOR."pages".DIRECTORY_SEPARATOR."modelli-esposti.php";
         break;
     case "biglietti":
         $header = str_replace("*title*","Biglietti | Museo Ferrari",$header);
-        $header = str_replace("*description*","",$header);
-        $header = str_replace("*keywords*","",$header);
+        $header = str_replace("*description*","In questa pagina è possibile prenotare i biglietti per partecipare alle mostre  del museo. Il prezzo è di soli 15.00 € e i minori entrano gratis.",$header);
+        $header = str_replace("*keywords*","biglietti,museo,mostre,gratis",$header);
         $header = str_replace("*linkbiglietti*","<li id='currentLink'>Biglietti</li>",$header);
         $header = str_replace("*breadcrumbs*","Biglietti",$header);
         $page = dirname(__DIR__).DIRECTORY_SEPARATOR."pages".DIRECTORY_SEPARATOR."biglietti.php";
         break;
     case "info-e-contatti":
         $header = str_replace("*title*","Info e Contatti | Museo Ferrari",$header);
-        $header = str_replace("*description*","",$header);
-        $header = str_replace("*keywords*","",$header);
+        $header = str_replace("*description*","Qui vi sono le informazioni relative a come raggiungere il museo, i modi per contattarci e una mappa con la posizione del museo.",$header);
+        $header = str_replace("*keywords*","informazioni,contatti,mappa,posizione",$header);
         $header = str_replace("*linkinfo*","<li id='currentLink'>Info e Contatti</li>",$header);
         $header = str_replace("*breadcrumbs*","Info e Contatti",$header);
         $page = dirname(__DIR__).DIRECTORY_SEPARATOR."pages".DIRECTORY_SEPARATOR."info-e-contatti.php";
